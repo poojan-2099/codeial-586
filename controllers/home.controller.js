1b32e8ece26f0874a51241b361a42056fb01a5b9
samirpatel017 <53654947+samirpatel017@users.noreply.github.com>
2020-08-23 13:33:37 +0530

    commit for post
    commit for completing of post

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,10 +1,18 @@
+const Post = require('../models/post')
+
 //function for home page
 module.exports.home=function(req,res){
-    res.statusCode=200;
-    console.log(req.cookies);
-    return res.render('home.ejs',{
-        title:'Home'
-    })
+    Post.find({}).populate('user').exec((err,post)=>{
+        if(err){
+            req.flash('error_message',"Error Post Not Found");
+            console.log('post not found');
+            return;
+        }
+        return res.render('home.ejs',{
+            title:'Codeial | Home',
+            post_list:post
+        });
+    });
 };
 module.exports.postUser=function(req,res){
     res.statusCode=200;
